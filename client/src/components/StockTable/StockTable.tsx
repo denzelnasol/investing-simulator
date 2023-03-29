aeadfaf92cc2391ceb4c9a19a0e700cfbbda3ced
dnasol <dnasol@sfu.ca>
2023-03-29 02:11:34 -0700

    1. implement stock table 2. implement stock trade dialog to purchase stocks 3. add trade button to navbar 4. comment out ts config due to annoying type checking

---
+++ client/src/components/StockTable/StockTable.tsx
@@ -0,0 +1,162 @@
+import React, { useState, useEffect } from "react";
+
+// Components
+import { FilterMatchMode, FilterOperator } from 'primereact/api';
+import { DataTable } from "primereact/datatable";
+import { Column } from 'primereact/column';
+import { Symbol } from 'enums/Stock';
+import Button from 'components/PrimeReact/Button/Button';
+import InputText from 'components/PrimeReact/InputText/InputText';
+import StockTradeDialog from "components/StockTradeDialog/StockTradeDialog";
+
+// API
+import { getCurrentStockInfo } from "api/Stock/Stock";
+
+import './style.scss';
+
+const StockTable = ({ ...props }) => {
+
+  // ** useStates ** //
+  const [data, setData] = useState([]);
+  const [selectedStock, setSelectedStock] = useState(null);
+  const [stockFilters, setStockFilters] = useState(null);
+  const [isTradeSelected, setIsTradeSelected] = useState<boolean>(false);
+  const [globalFilterValue, setGlobalFilterValue] = useState<string>('');
+
+  // ** useEffects ** //
+  useEffect(() => {
+    const symbols: Symbol[] = Object.values(Symbol).sort();
+    const fetchData = async () => {
+      const stockData = await getCurrentStockInfo(symbols);
+      setData(stockData);
+    };
+
+    fetchData();
+    initializeFilters();
+  }, []);
+
+  // ** Callback Functions ** //
+
+  const initializeFilters = () => {
+    setStockFilters({
+      'global': { value: null, matchMode: FilterMatchMode.CONTAINS },
+      'symbol': { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
+    });
+    setGlobalFilterValue('');
+  }
+
+  const onGlobalFilterChange1 = (e) => {
+    const value = e.target.value || null;
+    let _filters1 = { ...stockFilters };
+    _filters1['global'].value = value;
+
+    setStockFilters(_filters1);
+    setGlobalFilterValue(value);
+  }
+
+  const onStockSelect = (event) => {
+    setSelectedStock(event.data);
+  };
+
+  const clearStockFilter = () => {
+    initializeFilters();
+  }
+
+  // ** Components ** //
+
+  const renderTableHeader = () => {
+    return (
+      <div>
+        <div className="font-medium text-4xl mb-4">Trading Table</div>
+        <div className="flex justify-content-between">
+          <Button type="button" icon="pi pi-filter-slash" label="Clear" className="p-button-outlined" onClick={clearStockFilter} />
+          <span>
+            <InputText value={globalFilterValue} onChange={onGlobalFilterChange1} placeholder="Keyword Search" />
+          </span>
+        </div>
+      </div>
+    );
+  }
+  const tableHeader = renderTableHeader();
+
+  const priceRecentChangeColumn = (rowData) => {
+    return (
+      <>
+        <span>{rowData.regularMarketPrice}</span>
+        <span className={rowData.regularMarketChange > 0 ? "green" : "red"}>
+          {rowData.regularMarketChange.fmt} ({Math.round(rowData.regularMarketChange * 100) / 1000})
+        </span>
+      </>
+    );
+  }
+
+  const marketOpenColumn = (rowData) => {
+    return (
+      <span className={rowData.regularMarketOpen > rowData.regularMarketPreviousClose ? "green" : "red"}>
+        {rowData.regularMarketOpen}
+      </span>
+    );
+  }
+
+  const tradeColumn = (rowData) => {
+    return (
+      <Button label="Trade" onClick={() => {
+        setSelectedStock(rowData)
+        setIsTradeSelected(true)
+      }
+      }
+      />
+    );
+  }
+
+  return (
+    <>
+      <StockTradeDialog
+        stock={selectedStock}
+        displayTradeDialog={isTradeSelected}
+        hideTradeDialog={() => setIsTradeSelected(false)}
+      />
+      <DataTable
+        value={data}
+        selectionMode="single"
+        header={tableHeader}
+        onSelectionChange={onStockSelect}
+        filters={stockFilters}
+        filterDisplay="menu"
+        globalFilterFields={['symbol']}
+        emptyMessage="No stocks found."
+        paginator
+        rows={15}
+      >
+        <Column
+          field="symbol"
+          header="Symbol"
+          filter
+          filterPlaceholder="Search by name"
+          sortable />
+        <Column
+          header="Price/ Recent Change"
+          body={priceRecentChangeColumn}
+        />
+        <Column
+          header="Market Open"
+          body={marketOpenColumn}
+        />
+        <Column
+          field="regularMarketPreviousClose"
+          header="Previous Market Close"
+        />
+        <Column
+          header="Analyst Rating"
+          field="averageAnalystRating"
+          sortable
+        />
+        <Column
+          body={tradeColumn}
+        />
+      </DataTable>
+    </>
+  )
+}
+
+export default StockTable;
