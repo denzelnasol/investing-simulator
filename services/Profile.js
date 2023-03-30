e8a6972e1c4c58a6dc30af34a240fce66c57c51c
hln5 <harrynguyen019@gmail.com>
2023-03-29 19:26:01 -0700

    create separate function to query profile table with unhashed password

--- services/Profile.js
+++ services/Profile.js
@@ -8,12 +8,25 @@ async function getProfile(email, password) {
         where: {
             AND: {
                 email: email,
-                //password_hash: md5(password) // some passwords in database are not hashed
-                password_hash: password
+                password_hash: md5(password)
             },
         }
     });
 };
+
+// to be used only for development purposes
+// (some passwords in db are unhashed so if you want to log in
+//  with those profiles, use this function)
+async function getProfileUnhashed(email, password) {
+    return await prisma.profile.findFirst({
+        where: {
+            AND: {
+                email: email,
+                password_hash: password
+            },
+        }
+    });
+}
 
 async function getProfileByEmail(email) {
     return await prisma.profile.findFirst({
@@ -40,5 +53,6 @@ async function createProfile(firstName, lastName, email, password, phoneNumber) 
 module.exports = {
     getProfile,
     getProfileByEmail,
+    getProfileUnhashed,
     createProfile
 }
