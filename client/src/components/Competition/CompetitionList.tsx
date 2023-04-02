c85e28ff6d5901e9dfbd80a38d0223870276efdf
dnasol <dnasol@sfu.ca>
2023-04-02 01:24:05 -0700

    style competition table list

--- client/src/components/Competition/CompetitionList.tsx
+++ client/src/components/Competition/CompetitionList.tsx
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
-import { Button } from 'primereact/button';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+
+// API
 import { getCompetitionPortfolios } from 'api/Profile/User';
+
+// Components
+import { Button } from 'primereact/button';
+import { DataTable } from "primereact/datatable";
+import { Column } from 'primereact/column';
+import { getCompetitionData } from 'api/Competition/Competition';
 
 interface Competition {
     name: string,
@@ -16,7 +23,9 @@ interface FetchedPortfolio {
 }
 
 function CompetitionList(props) {
+    const navigate = useNavigate();
     const [competitions, setCompetitions] = useState<any>([]);
+    const [selectedCompetition, setSelectedCompetition] = useState<any>(null);
 
     useEffect(() => {
         async function fetchData() {
@@ -25,30 +34,54 @@ function CompetitionList(props) {
             const portfolios = await getCompetitionPortfolios(token);
             console.log(portfolios);
 
+            if (!portfolios) return;
+
             // update list of competitions on UI
-            const competitionLinks = portfolios.map((p: FetchedPortfolio) => {                
+            const competitionLinks = await Promise.all(portfolios.map(async (p) => {
                 const competitionName = p.competition.name;
                 const competitionId = p.fk_competition;
+                const competitionData = await getCompetitionData(competitionId);
+                const startDate = new Date(competitionData.competitionStart).toLocaleString();
+                const endDate = new Date(competitionData.competitionEnd).toLocaleString();
                 return (
-                    <li key={competitionId}>
-                        <Link to={`/competition?id=${competitionId}`} style={{ textDecoration: 'none' }}>
-                            <Button label={competitionName} />
-                        </Link>
-                    </li>
+                    {
+                        competitionName,
+                        competitionId,
+                        startDate,
+                        endDate
+                    }
                 )
-            });
+            }));
             setCompetitions(competitionLinks);
         }
 
         fetchData();
     }, []);
 
+    useEffect(() => {
+        if (selectedCompetition) {
+            navigate(`/competition?id=${selectedCompetition.competitionId}`);
+        }
+
+    }, [selectedCompetition]);
+
+    const competitionListTable = (
+        <DataTable
+            value={competitions}
+            selectionMode="single"
+            selection={selectedCompetition}
+            onSelectionChange={e => setSelectedCompetition(e.value)}
+        >
+            <Column field="competitionName" header="Name"></Column>
+            <Column field="startDate" header="Start Date"></Column>
+            <Column field="endDate" header="End Date"></Column>
+        </DataTable>
+    );
+
     return (
         <div>
             <h1>Your Ongoing Competitions</h1>
-            <ul style={{listStyleType: 'none'}}>
-                {competitions}
-            </ul>
+            {competitionListTable}
         </div>
     );
 };
