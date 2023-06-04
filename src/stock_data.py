c4718a4af46589894d450302299a6a1ec16037d3
dnasol <dnasol@sfu.ca>
2023-06-04 14:22:24 -0700

    fix

--- src/stock_data.py
+++ src/stock_data.py
@@ -10,16 +10,13 @@ def get_stock_data(symbols):
   stock_data = {}  # Object to store ticker info
 
   for ticker_symbol, ticker_obj in tickers.tickers.items():
-    try:
       stock_data[ticker_symbol] = ticker_obj.info
-    except HTTPError as e:
-      print(f"Error retrieving data for symbol {ticker_symbol}: {e}")
-      stock_data[ticker_symbol] = None
 
   stock_data_json = json.dumps(stock_data)
   return stock_data_json
 
 symbols = sys.argv[1]
+
 
 
 print(get_stock_data(symbols))
