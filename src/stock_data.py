62019e2214a931da1807c72934c2d71a82e1c008
dnasol <dnasol@sfu.ca>
2023-06-04 15:04:40 -0700

    fix

--- src/stock_data.py
+++ src/stock_data.py
@@ -1,24 +1,21 @@
 import sys
 import json
+from urllib.error import HTTPError
 import yfinance as yf
-from requests.exceptions import HTTPError
 
 def get_stock_data(symbols):
-    symbols = symbols.replace(',', ' ')
-    tickers = yf.Tickers(symbols)
+  symbols = symbols.replace(',', ' ')
+  tickers = yf.Tickers(symbols)
 
-    stock_data = {}  # Object to store ticker info
+  stock_data = {}  # Object to store ticker info
 
-    for ticker_symbol, ticker_obj in tickers.tickers.items():
-        try:
-            stock_data[ticker_symbol] = ticker_obj.info
-        except HTTPError as e:
-            # print(f"Error retrieving data for symbol {ticker_symbol}: {e}")
-            continue
+  for ticker_symbol, ticker_obj in tickers.tickers.items():
+      stock_data[ticker_symbol] = ticker_obj.info
 
-    print(stock_data)
-    stock_data_json = json.dumps(stock_data)
-    return stock_data_json
+  stock_data_json = json.dumps(stock_data)
+  return stock_data_json
 
 symbols = sys.argv[1]
-get_stock_data(symbols)
+
+
+print(get_stock_data(symbols))
