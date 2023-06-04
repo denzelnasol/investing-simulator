19415d4f434f0d02778b21399fa930a5b858fe3e
dnasol <dnasol@sfu.ca>
2023-06-04 13:34:41 -0700

    test fix

--- services/StockApi.js
+++ services/StockApi.js
@@ -30,23 +30,13 @@ async function getRTStockDetails(symbol, fields = []) {
     return await yahooFinance.quote(symbol.toUpperCase(), { fields: fields });
 }
 
-function getYFStockSymbols(symbols, callback) {
+async function getYFStockSymbols(symbols, callback) {
     const symbolString = symbols.join(',');
 
     const command = `python3 stock_data.py ${symbolString}`;
   
-    exec(command, (error, stdout, stderr) => {
-    //   if (error) {
-    //     console.error(`Error executing Python script: ${error}`);
-    //     return res.status(500).json({ error: 'Internal Server Error' });
-    //   }
-    //   if (stderr) {
-    //     console.error(`Python script returned an error: ${stderr}`);
-    //     return res.status(500).json({ error: 'Internal Server Error' });
-    //   }
-      
-      callback(null, stdout);
-    });
+    const { stdout } = await exec(command, (error, stdout, stderr));
+    return stdout;
 }
 
 module.exports = {
